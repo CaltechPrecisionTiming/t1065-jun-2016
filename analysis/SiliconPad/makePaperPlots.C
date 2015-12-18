65f16a0dd976020e7c79c4eb6583487d7abf8f35
Artur Apresyan <apresyan@gmail.com>
2015-12-18 18:12:03 +0100

    make paper plots

---
+++ analysis/SiliconPad/makePaperPlots.C
@@ -0,0 +1,288 @@
+// make the plots as a function of beam energy
+/*void makePaperPlots(){
+
+  TFile *_file0 = TFile::Open("run112_lin30_DeltaT.root");
+  TFile *_file1 = TFile::Open("run109_lin30_DeltaT.root");
+  TFile *_file2 = TFile::Open("run110_lin30_DeltaT.root");
+  TFile *_file3 = TFile::Open("run114_lin30_DeltaT.root");
+
+  TH1F* deltaT0 = (TH1F*)_file0->Get("DeltaT");
+  TH1F* deltaT1 = (TH1F*)_file1->Get("DeltaT");
+  TH1F* deltaT2 = (TH1F*)_file2->Get("DeltaT");
+  TH1F* deltaT3 = (TH1F*)_file3->Get("DeltaT");
+
+  TCanvas *c = new TCanvas ("c","c",800, 600);
+  TLatex *tex = new TLatex();
+  tex->SetNDC();
+  tex->SetTextSize(0.060);
+  tex->SetTextFont(42);
+  tex->SetTextColor(kBlack);
+
+  c->cd();
+  gStyle->SetOptStat(0);
+  gPad->SetGridx();
+  gPad->SetGridy();
+ 
+
+  deltaT0->Draw();
+  TF1* fgaus0 = new TF1("fgaus0","gaus", deltaT0->GetMean() - 1.5*deltaT0->GetRMS(), deltaT0->GetMean() + 1.5*deltaT0->GetRMS());
+  deltaT0->Fit("fgaus0","Q","", deltaT0->GetMean() - 1.5*deltaT0->GetRMS(), deltaT0->GetMean() + 1.5*deltaT0->GetRMS());
+  float res0 = fgaus0->GetParameter(2);
+  float error0 = fgaus0->GetParError(2);
+  deltaT0->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT0->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus0->GetParameter(2), 1000*fgaus0->GetParError(2)));
+  c->SaveAs("deltaT0.pdf");
+
+  deltaT1->Draw();
+  TF1* fgaus1 = new TF1("fgaus1","gaus", deltaT1->GetMean() - 1.5*deltaT1->GetRMS(), deltaT1->GetMean() + 1.5*deltaT1->GetRMS());
+  deltaT1->Fit("fgaus1","Q","", deltaT1->GetMean() - 1.5*deltaT1->GetRMS(), deltaT1->GetMean() + 1.5*deltaT1->GetRMS());
+  float res1 = fgaus1->GetParameter(2);
+  float error1 = fgaus1->GetParError(2);
+  deltaT1->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT1->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus1->GetParameter(2), 1000*fgaus1->GetParError(2)));
+  c->SaveAs("deltaT1.pdf");
+
+  deltaT2->Draw();
+  TF1* fgaus2 = new TF1("fgaus2","gaus", deltaT2->GetMean() - 1.5*deltaT2->GetRMS(), deltaT2->GetMean() + 1.5*deltaT2->GetRMS());
+  deltaT2->Fit("fgaus2","Q","", deltaT2->GetMean() - 1.5*deltaT2->GetRMS(), deltaT2->GetMean() + 1.5*deltaT2->GetRMS());
+  float res2 = fgaus2->GetParameter(2);
+  float error2 = fgaus2->GetParError(2);
+  deltaT2->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT2->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus2->GetParameter(2), 1000*fgaus2->GetParError(2)));
+  c->SaveAs("deltaT2.pdf");
+
+  deltaT3->Draw();
+  TF1* fgaus3 = new TF1("fgaus3","gaus", deltaT3->GetMean() - 1.5*deltaT3->GetRMS(), deltaT3->GetMean() + 1.5*deltaT3->GetRMS());
+  deltaT3->Fit("fgaus3","Q","", deltaT3->GetMean() - 1.5*deltaT3->GetRMS(), deltaT3->GetMean() + 1.5*deltaT3->GetRMS());
+  float res3 = fgaus3->GetParameter(2);
+  float error3 = fgaus3->GetParError(2);
+  deltaT3->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT3->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus3->GetParameter(2), 1000*fgaus3->GetParError(2)));
+  c->SaveAs("deltaT3.pdf");
+
+  float res[4]    = {1000*res0, 1000*res1, 1000*res2, 1000*res3}; 
+  float charge[4] = {4., 8, 16, 32};
+  float errorX[4] = {0.}; 
+  float errorY[4] = {1000*error0, 1000*error1, 1000*error2, 1000*error3};
+
+  TGraphErrors* gr = new TGraphErrors( 4, charge, res, errorX, errorY );
+  gr -> SetTitle("");
+
+  gr -> SetMarkerStyle(8);
+  gr -> SetLineColor(kBlue);
+  gr -> SetMarkerColor(kBlue);
+  gr -> SetMarkerSize(0.9);
+  gr -> SetLineWidth(2);
+
+  gr -> Draw("APL");
+
+  gr->GetXaxis()->SetTitle("Beam Energy [GeV]");
+  gr->GetYaxis()->SetTitle("Time Resolution [ps]");
+  gr->GetXaxis()->SetTitleSize(0.045);
+  gr->GetXaxis()->SetLabelSize(0.045);
+  gr->GetXaxis()->SetTitleOffset(1.0);
+  gr->GetYaxis()->SetTitleOffset(0.95);
+  gr->GetYaxis()->SetTitleSize(0.05);
+  gr->GetYaxis()->SetLabelSize(0.045);
+  gr->GetXaxis()->SetRangeUser(0.0, 700);
+  gr->GetYaxis()->SetRangeUser(0., 80.);
+ 
+  c->SaveAs("SigmaT_vs_BeamEnergy_lin30Stamp.pdf");
+}
+*/
+/*
+// make the plots as a function of X0
+void makePaperPlots(){
+
+  TFile *_file0 = TFile::Open("run119_lin30_DeltaT.root");
+  TFile *_file1 = TFile::Open("run118_lin30_DeltaT.root");
+  TFile *_file2 = TFile::Open("run117_lin30_DeltaT.root");
+  TFile *_file3 = TFile::Open("run116_lin30_DeltaT.root");
+
+  TH1F* deltaT0 = (TH1F*)_file0->Get("DeltaT");
+  TH1F* deltaT1 = (TH1F*)_file1->Get("DeltaT");
+  TH1F* deltaT2 = (TH1F*)_file2->Get("DeltaT");
+  TH1F* deltaT3 = (TH1F*)_file3->Get("DeltaT");
+
+  TCanvas *c = new TCanvas ("c","c",800, 600);
+  TLatex *tex = new TLatex();
+  tex->SetNDC();
+  tex->SetTextSize(0.060);
+  tex->SetTextFont(42);
+  tex->SetTextColor(kBlack);
+
+  c->cd();
+  gStyle->SetOptStat(0);
+  gPad->SetGridx();
+  gPad->SetGridy();
+ 
+
+  deltaT0->Draw();
+  TF1* fgaus0 = new TF1("fgaus0","gaus", deltaT0->GetMean() - 1.5*deltaT0->GetRMS(), deltaT0->GetMean() + 1.5*deltaT0->GetRMS());
+  deltaT0->Fit("fgaus0","Q","", deltaT0->GetMean() - 1.5*deltaT0->GetRMS(), deltaT0->GetMean() + 1.5*deltaT0->GetRMS());
+  float res0 = fgaus0->GetParameter(2);
+  float error0 = fgaus0->GetParError(2);
+  deltaT0->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT0->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus0->GetParameter(2), 1000*fgaus0->GetParError(2)));
+  c->SaveAs("deltaT_1X0.pdf");
+
+  deltaT1->Draw();
+  TF1* fgaus1 = new TF1("fgaus1","gaus", deltaT1->GetMean() - 1.5*deltaT1->GetRMS(), deltaT1->GetMean() + 1.5*deltaT1->GetRMS());
+  deltaT1->Fit("fgaus1","Q","", deltaT1->GetMean() - 1.5*deltaT1->GetRMS(), deltaT1->GetMean() + 1.5*deltaT1->GetRMS());
+  float res1 = fgaus1->GetParameter(2);
+  float error1 = fgaus1->GetParError(2);
+  deltaT1->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT1->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus1->GetParameter(2), 1000*fgaus1->GetParError(2)));
+  c->SaveAs("deltaT_2X0.pdf");
+
+  deltaT2->Draw();
+  TF1* fgaus2 = new TF1("fgaus2","gaus", deltaT2->GetMean() - 1.5*deltaT2->GetRMS(), deltaT2->GetMean() + 1.5*deltaT2->GetRMS());
+  deltaT2->Fit("fgaus2","Q","", deltaT2->GetMean() - 1.5*deltaT2->GetRMS(), deltaT2->GetMean() + 1.5*deltaT2->GetRMS());
+  float res2 = fgaus2->GetParameter(2);
+  float error2 = fgaus2->GetParError(2);
+  deltaT2->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT2->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus2->GetParameter(2), 1000*fgaus2->GetParError(2)));
+  c->SaveAs("deltaT_4X0.pdf");
+
+  deltaT3->Draw();
+  TF1* fgaus3 = new TF1("fgaus3","gaus", deltaT3->GetMean() - 1.5*deltaT3->GetRMS(), deltaT3->GetMean() + 1.5*deltaT3->GetRMS());
+  deltaT3->Fit("fgaus3","Q","", deltaT3->GetMean() - 1.5*deltaT3->GetRMS(), deltaT3->GetMean() + 1.5*deltaT3->GetRMS());
+  float res3 = fgaus3->GetParameter(2);
+  float error3 = fgaus3->GetParError(2);
+  deltaT3->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT3->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus3->GetParameter(2), 1000*fgaus3->GetParError(2)));
+  c->SaveAs("deltaT_6X0.pdf");
+
+  float res[4]    = {1000*res0, 1000*res1, 1000*res2, 1000*res3}; 
+  float charge[4] = {1., 2, 4, 6};
+  float errorX[4] = {0.}; 
+  float errorY[4] = {1000*error0, 1000*error1, 1000*error2, 1000*error3};
+
+  TGraphErrors* gr = new TGraphErrors( 4, charge, res, errorX, errorY );
+  gr -> SetTitle("");
+
+  gr -> SetMarkerStyle(8);
+  gr -> SetLineColor(kBlue);
+  gr -> SetMarkerColor(kBlue);
+  gr -> SetMarkerSize(0.9);
+  gr -> SetLineWidth(2);
+
+  gr -> Draw("APL");
+
+  gr->GetXaxis()->SetTitle("Tungsten Absober Thickness [X0]");
+  gr->GetYaxis()->SetTitle("Time Resolution [ps]");
+  gr->GetXaxis()->SetTitleSize(0.045);
+  gr->GetXaxis()->SetLabelSize(0.045);
+  gr->GetXaxis()->SetTitleOffset(1.0);
+  gr->GetYaxis()->SetTitleOffset(0.95);
+  gr->GetYaxis()->SetTitleSize(0.05);
+  gr->GetYaxis()->SetLabelSize(0.045);
+  gr->GetXaxis()->SetRangeUser(0.0, 700);
+  gr->GetYaxis()->SetRangeUser(0., 150.);
+ 
+  c->SaveAs("SigmaT_vs_X0_lin30Stamp.pdf");
+}
+*/
+
+// make the plots as a function of Silicon Voltage
+void makePaperPlots(){
+
+  TFile *_file0 = TFile::Open("run111-234_lin30_DeltaT.root");
+  TFile *_file1 = TFile::Open("run111-1_lin30_DeltaT.root");
+  TFile *_file2 = TFile::Open("run111-0_lin30_DeltaT.root");
+  TFile *_file3 = TFile::Open("run110_lin30_DeltaT.root");
+
+  TH1F* deltaT0 = (TH1F*)_file0->Get("DeltaT");
+  TH1F* deltaT1 = (TH1F*)_file1->Get("DeltaT");
+  TH1F* deltaT2 = (TH1F*)_file2->Get("DeltaT");
+  TH1F* deltaT3 = (TH1F*)_file3->Get("DeltaT");
+
+  TCanvas *c = new TCanvas ("c","c",800, 600);
+  TLatex *tex = new TLatex();
+  tex->SetNDC();
+  tex->SetTextSize(0.060);
+  tex->SetTextFont(42);
+  tex->SetTextColor(kBlack);
+
+  c->cd();
+  gStyle->SetOptStat(0);
+  gPad->SetGridx();
+  gPad->SetGridy();
+
+  deltaT0->Draw();
+  TF1* fgaus0 = new TF1("fgaus0","gaus", deltaT0->GetMean() - 1.0*deltaT0->GetRMS(), deltaT0->GetMean() + 1.0*deltaT0->GetRMS());
+  deltaT0->Fit("fgaus0","Q","", deltaT0->GetMean() - 1.0*deltaT0->GetRMS(), deltaT0->GetMean() + 1.0*deltaT0->GetRMS());
+  float res0 = fgaus0->GetParameter(2);
+  float error0 = fgaus0->GetParError(2);
+  deltaT0->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT0->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus0->GetParameter(2), 1000*fgaus0->GetParError(2)));
+  c->SaveAs("deltaT_200V.pdf");
+
+  deltaT1->Draw();
+  TF1* fgaus1 = new TF1("fgaus1","gaus", deltaT1->GetMean() - 1.0*deltaT1->GetRMS(), deltaT1->GetMean() + 1.0*deltaT1->GetRMS());
+  deltaT1->Fit("fgaus1","Q","", deltaT1->GetMean() - 1.0*deltaT1->GetRMS(), deltaT1->GetMean() + 1.0*deltaT1->GetRMS());
+  float res1 = fgaus1->GetParameter(2);
+  float error1 = fgaus1->GetParError(2);
+  deltaT1->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT1->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus1->GetParameter(2), 1000*fgaus1->GetParError(2)));
+  c->SaveAs("deltaT_300V.pdf");
+
+  deltaT2->Draw();
+  TF1* fgaus2 = new TF1("fgaus2","gaus", deltaT2->GetMean() - 1.0*deltaT2->GetRMS(), deltaT2->GetMean() + 1.0*deltaT2->GetRMS());
+  deltaT2->Fit("fgaus2","Q","", deltaT2->GetMean() - 1.0*deltaT2->GetRMS(), deltaT2->GetMean() + 1.0*deltaT2->GetRMS());
+  float res2 = fgaus2->GetParameter(2);
+  float error2 = fgaus2->GetParError(2);
+  deltaT2->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT2->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus2->GetParameter(2), 1000*fgaus2->GetParError(2)));
+  c->SaveAs("deltaT_400V.pdf");
+
+  deltaT3->Draw();
+  TF1* fgaus3 = new TF1("fgaus3","gaus", deltaT3->GetMean() - 1.0*deltaT3->GetRMS(), deltaT3->GetMean() + 1.0*deltaT3->GetRMS());
+  deltaT3->Fit("fgaus3","Q","", deltaT3->GetMean() - 1.0*deltaT3->GetRMS(), deltaT3->GetMean() + 1.0*deltaT3->GetRMS());
+  float res3 = fgaus3->GetParameter(2);
+  float error3 = fgaus3->GetParError(2);
+  deltaT3->GetXaxis()->SetTitle("Time Resolution [ps]");
+  deltaT3->SetTitle("");
+  tex->DrawLatex(0.6, 0.80, Form("#sigma = %.0f #pm %.2f ps", 1000*fgaus3->GetParameter(2), 1000*fgaus3->GetParError(2)));
+  c->SaveAs("deltaT_500V.pdf");
+
+  float res[4]    = {1000*res0, 1000*res1, 1000*res2, 1000*res3}; 
+  float charge[4] = {200., 300., 400., 500.};
+  float errorX[4] = {0.}; 
+  float errorY[4] = {1000*error0, 1000*error1, 1000*error2, 1000*error3};
+
+  TGraphErrors* gr = new TGraphErrors( 4, charge, res, errorX, errorY );
+  gr -> SetTitle("");
+
+  gr -> SetMarkerStyle(8);
+  gr -> SetLineColor(kBlue);
+  gr -> SetMarkerColor(kBlue);
+  gr -> SetMarkerSize(0.9);
+  gr -> SetLineWidth(2);
+
+  gr -> Draw("APL");
+
+  gr->GetXaxis()->SetTitle("Voltage ");
+  gr->GetYaxis()->SetTitle("Time Resolution [ps]");
+  gr->GetXaxis()->SetTitleSize(0.045);
+  gr->GetXaxis()->SetLabelSize(0.045);
+  gr->GetXaxis()->SetTitleOffset(1.0);
+  gr->GetYaxis()->SetTitleOffset(0.95);
+  gr->GetYaxis()->SetTitleSize(0.05);
+  gr->GetYaxis()->SetLabelSize(0.045);
+  gr->GetXaxis()->SetRangeUser(0.0, 700);
+  gr->GetYaxis()->SetRangeUser(0., 80.);
+ 
+  c->SaveAs("SigmaT_vs_DV_lin30Stamp.pdf");
+}
---
+++ doc/papers/SiliconPad/scripts/makeROOTFiles.sh
@@ -0,0 +1,45 @@
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run112-500V_123.root run112 0.02 gaus
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run112-500V_123.root run112 0.02 lin0
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run112-500V_123.root run112 0.02 lin15
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run112-500V_123.root run112 0.02 lin30
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run112-500V_123.root run112 0.02 lin45
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run112-500V_123.root run112 0.02 lin60
+
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run109.root run109 0.05 gaus
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run109.root run109 0.05 lin0
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run109.root run109 0.05 lin15
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run109.root run109 0.05 lin30
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run109.root run109 0.05 lin45
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run109.root run109 0.05 lin60
+
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run110.root run110 0.1 gaus
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run110.root run110 0.1 lin0
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run110.root run110 0.1 lin15
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run110.root run110 0.1 lin30
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run110.root run110 0.1 lin45
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run110.root run110 0.1 lin60
+
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run114.root run114 0.1 gaus
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run114.root run114 0.1 lin0
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run114.root run114 0.1 lin15
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run114.root run114 0.1 lin30
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run114.root run114 0.1 lin45
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run114.root run114 0.1 lin60
+
+
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run116.root run116 0.05 lin30
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run117.root run117 0.05 lin30
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run118.root run118 0.03 lin30
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run119.root run119 0.03 lin30
+
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run116.root run116 0.05 gaus
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run117.root run117 0.05 gaus
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run118.root run118 0.03 gaus
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run119.root run119 0.03 gaus
+
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run111-0.dat-full.root run111-0 0.1 lin30
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run111-1.dat-full.root run111-1 0.1 lin30
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run111_200V-234.root run111-234 0.1 lin30
+./makeSigma eos/cms/store/group/phys_susy/razor/Timing/t1065-dec-2015/reco/v2/t1065_dec2015_run111_600V-567.root run111-567 0.1 lin30
+
+
