16b82f6399388c67f903a718a1361feac349eb84
Si Xie <si.xie@cern.ch>
2016-06-21 19:15:32 -0500

    update

--- analysis/PicoSil/MultiChannelStudy_TimingMethod1.C
+++ analysis/PicoSil/MultiChannelStudy_TimingMethod1.C
@@ -225,7 +225,10 @@ void DoMultiChannelStudy( string filename , string outputFilename) {
   // these are histograms to compare channels pairwise
   TH1F* histTOF2Pixel12_C = new TH1F("histTOF__Pixel_C","; Time [ns];Number of Events", 200, -1,1);
   TH1F* histTOF2Pixel13_C = new TH1F("histTOF__Pixel2_C","; Time [ns];Number of Events", 200, -1,1);
-
+  TH1F* histTOF2Pixel14_C = new TH1F("histTOF__Pixel3_C","; Time [ns];Number of Events", 200, -1,1);
+  TH1F* histTOF2Pixel15_C = new TH1F("histTOF__Pixel4_C","; Time [ns];Number of Events", 200, -1,1);
+  TH1F* histTOF2Pixel16_C = new TH1F("histTOF__Pixel5_C","; Time [ns];Number of Events", 200, -1,1);
+  TH1F* histTOF2Pixel17_C = new TH1F("histTOF__Pixel6_C","; Time [ns];Number of Events", 200, -1,1);
 
   histTOFFlatAvg_C = new TH1F("histTOFFlatAvg_C","; Time [ns];Number of Events", 200, -6,-4);
   histTOFChargeWeightedAvg_C = new TH1F("histTOFChargeWeightedAvg_C","; Time [ns];Number of Events", 200, -1,-1);
@@ -276,6 +279,15 @@ void DoMultiChannelStudy( string filename , string outputFilename) {
 	  histTOF2Pixel12_C->Fill( average  );
 	  average = (vect[0].charge*vect[0].time + vect[2].charge*vect[2].time )/(vect[0].charge+vect[2].charge);
 	  histTOF2Pixel13_C->Fill( average  );
+	  average  = (vect[0].charge*vect[0].time + vect[3].charge*vect[3].time )/(vect[0].charge+vect[3].charge);
+	  histTOF2Pixel14_C->Fill( average  );
+	  average = (vect[0].charge*vect[0].time + vect[4].charge*vect[4].time )/(vect[0].charge+vect[4].charge);
+	  histTOF2Pixel15_C->Fill( average  );
+	  average  = (vect[0].charge*vect[0].time + vect[5].charge*vect[5].time )/(vect[0].charge+vect[5].charge);
+	  histTOF2Pixel16_C->Fill( average  );
+	  average = (vect[0].charge*vect[0].time + vect[6].charge*vect[6].time )/(vect[0].charge+vect[6].charge);
+	  histTOF2Pixel17_C->Fill( average  );
+	  // 1 is the center channel of the pico sil, 2-7 are the first ring
 	}
       std::cout << "event: " << iEntry << "-->" << vect[0].charge << " " << vect[1].charge << " " << vect[2].charge << std::endl;
     }
@@ -322,12 +334,15 @@ void DoMultiChannelStudy( string filename , string outputFilename) {
 
   file->WriteTObject(histTOF2Pixel12_C,"AveragePixels12", "WriteDelete");
   file->WriteTObject(histTOF2Pixel13_C,"AveragePixels13", "WriteDelete");
-
+  file->WriteTObject(histTOF2Pixel14_C,"AveragePixels14", "WriteDelete");
+  file->WriteTObject(histTOF2Pixel15_C,"AveragePixels15", "WriteDelete");  
+  file->WriteTObject(histTOF2Pixel16_C,"AveragePixels16", "WriteDelete");
+  file->WriteTObject(histTOF2Pixel17_C,"AveragePixels17", "WriteDelete");
       
   for( int i = 0; i < 7; i++ )
     {
-      histDeltaT_C[i]->Write( Form("deltaT_%d_corr", i) );
-      histDeltaT[i]->Write( Form("deltaT_%d", i) );
+      histDeltaT_C[i]->Write( Form("deltaT_%d_corr", i+1) );
+      histDeltaT[i]->Write( Form("deltaT_%d", i+1) );
     }
   
   file->Close();
